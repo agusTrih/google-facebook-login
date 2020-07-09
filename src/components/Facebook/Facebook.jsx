3ae22110fb771f06e02a8a240b7d3697f472082f
agusTrih <ant3dong@gmail.com>
2020-07-09 13:10:54 +0700

    add response in alert

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { useHisory } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import swal from "sweetalert";
 import { FacebookProvider, Login } from "react-facebook";
+import { useHistory } from "react-router-dom";
+
 function Facebook() {
     const history = useHistory();
     const [facebook, setFacebook] = useState({});
@@ -13,27 +15,43 @@ function Facebook() {
     };
     useEffect(() => {
         if (facebook.profile !== undefined) {
+            swal({
+                title: "Good job!",
+                text: `Selamat datang 
+                Nama: ${facebook.profile.name}
+                email: ${facebook.profile.email}`,
+
+                icon: "success",
+                button: "hokay!",
+            });
             history.push("/home");
+
+            console.log(facebook.profile, "consolenih");
         }
     }, [facebook, history]);
 
     return (
-        <div>
-            <FacebookProvider appId="2697154960539775">
-                <Login
-                    scope="email"
-                    onCompleted={handleResponse}
-                    onError={handleError}
-                >
-                    {({ loading, handleClick, error, data }) => (
+        <FacebookProvider appId="2697154960539775">
+            <Login
+                scope="email"
+                onCompleted={handleResponse}
+                onError={handleError}
+            >
+                {({ loading, handleClick, error, data }) => (
+                    <button>
+                        {" "}
                         <span onClick={handleClick}>
                             Login via Facebook
-                            {loading && <span>Loading...</span>}
+                            {loading && (
+                                <span style={{ color: "pink" }}>
+                                    Loading...
+                                </span>
+                            )}
                         </span>
-                    )}
-                </Login>
-            </FacebookProvider>
-        </div>
+                    </button>
+                )}
+            </Login>
+        </FacebookProvider>
     );
 }
 
---
+++ src/components/Google/Google.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { GoogleLogin } from "react-google-login";
+import { useHistory } from "react-router-dom";
+import swal from "sweetalert";
+
+export default function Google() {
+    const history = useHistory();
+    const responseGoogle = (response) => {
+        if (response !== null) {
+            swal({
+                title: "Good job!",
+                text: `Selamat datang 
+                Nama: ${response.profileObj.name}
+                email: ${response.profileObj.email}`,
+                icon: "success",
+                button: "hokay!",
+            });
+
+            history.push("/home");
+        }
+        console.log(response.profileObj.name, "ini log google");
+    };
+
+    return (
+        <GoogleLogin
+            clientId="300123951755-j76ddtd9b2cboqcitch33f7sl53gcc25.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+        />
+    );
+}
